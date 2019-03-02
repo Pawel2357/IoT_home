dcf32444bfe99a969f12b7ce7798362e4d70e22f
Pawel Urbanski <2pawel.urbanski@gmail.com>
2019-03-02 17:10:07 +0100

    Write code for working Arduino Uno subscriber.

---
+++ Readme.md
@@ -0,0 +1,14 @@
+How to set up Node MCU v3:
+1. Go to Arduino IDE
+2. Set board to NodeMCU 1.0
+3. Connect to right port
+4. Upload the sketch node_mcu_code.ino
+
+How to set up Arduino Uno:
+1. Go to Arduino IDE
+2. Set board to Arduino Uno
+3. Connect to right port
+4. Upload the sketch arduino_uno_pin_control.ino
+
+Remark:
+When uploading remove Rx and Tx conncetions.
---
+++ arduino_uno_pin_control.ino
@@ -0,0 +1,25 @@
+String a; //Initialized variable to store recieved data
+const int ledPin = 13;
+
+void setup() {
+  //Serial Begin at 9600 Baud 
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(ledPin, HIGH);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    a = Serial.readString();
+
+    // say what you got:
+    if (a == "0"){
+      digitalWrite(ledPin, LOW); 
+    }if (a == "1"){
+      digitalWrite(ledPin, HIGH);
+    }
+    Serial.println(a);
+  }
+}
