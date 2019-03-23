8b05e0347fa0e4c06a6272c589215d86165dfcf4
Pawel2357 <2pawel.urbanski@gmail.com>
2019-03-23 11:19:54 +0100

    Add script to control relay using esp node mcu.

---
+++ smart_relay.ino
@@ -0,0 +1,14 @@
+// circuit taken from https://www.raspberrypi.org/forums/viewtopic.php?t=36225&start=50
+
+int relayInput = 2; // the input to the relay pin
+
+void setup() {
+  pinMode(relayInput, OUTPUT); // initialize pin as OUTPUT
+}
+
+void loop() {
+  digitalWrite(relayInput, HIGH); // turn relay on
+  delay(1000);
+  digitalWrite(relayInput, LOW); // turn relay off
+  delay(1000);
+}
