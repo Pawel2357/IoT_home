5ed048794ca78da8e7aed038773372dbfc23ee98
Pawel2357 <2pawel.urbanski@gmail.com>
2019-03-27 20:59:11 +0100

    Correct measurements times.

--- 3d_printer_hub/trigger_publish.py
+++ 3d_printer_hub/trigger_publish.py
@@ -6,8 +6,8 @@ client.connect("40.121.36.126", 1883)
 
 while True:
     client.publish("sensors_3d_printing_room", "a")
-    time.sleep(30)
+    time.sleep(3)
     client.publish("sensors_3d_printing_room", "h")
-    time.sleep(30)
+    time.sleep(3)
     client.publish("sensors_3d_printing_room", "t")
-    time.sleep(30)
+    time.sleep(60)
