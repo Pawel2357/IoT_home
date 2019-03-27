353cac7774fb768c68a13d02075a8028db121851
Pawel2357 <2pawel.urbanski@gmail.com>
2019-03-27 20:40:08 +0100

    Add script to trigger data logging.

---
+++ 3d_printer_hub/trigger_publish.ino
@@ -0,0 +1,13 @@
+import paho.mqtt.client as mqtt
+import time
+
+client = mqtt.Client()
+client.connect("40.121.36.126", 1883)
+
+while True:
+    client.publish("sensors_3d_printing_room", "a")
+    time.sleep(30)
+    client.publish("sensors_3d_printing_room", "h")
+    time.sleep(30)
+    client.publish("sensors_3d_printing_room", "t")
+    time.sleep(30)
