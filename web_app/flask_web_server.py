ebd20c154c7280dcffd759e0e7b2f7f482e95a95
Pawel2357 <2pawel.urbanski@gmail.com>
2019-04-26 21:08:55 +0200

    Add flask web script.

---
+++ web_app/flask_web_server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import paho.mqtt.client as mqtt
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    print(request.method)
+    if request.method == 'POST':
+        if request.form.get('Turn on') == 'Turn on':
+            client = mqtt.Client()
+            client.connect("40.121.36.126", 1883)
+            client.publish("sensors_3d_printing_room", "0")
+            time.sleep(1)
+        elif  request.form.get('Turn off') == 'Turn off':
+            client = mqtt.Client()
+            client.connect("40.121.36.126", 1883)
+            client.publish("sensors_3d_printing_room", "1")
+            time.sleep(1)
+        else:
+            # pass # unknown
+            with open("/home/pawel2357/3d_printer.txt") as f:
+                data = f.readlines()
+            lastline = data[-1]
+            with open("/home/pawel2357/gps_data_correct.txt") as f:
+                data = f.readlines()
+            last_car = data[-1]
+            return render_template("index.html", value=lastline[-150:], car_value=last_car[-150:])
+    elif request.method == 'GET':
+        # return render_template("index.html")
+        print("No Post Back Call")
+    with open("/home/pawel2357/3d_printer.txt") as f:
+        data = f.readlines()
+    lastline = data[-1]
+    with open("/home/pawel2357/gps_data_correct.txt") as f:
+        data = f.readlines()
+    last_car = data[-1]
+    return render_template("index.html", value=lastline[-150:], car_value=last_car[-150:])
+    
+    if __name__ == '__main__':
+        app.run(host = '0.0.0.0')
