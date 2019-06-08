a985126b4054da706dc380a5811b4a5768c52b1f
Pawel2357 <2pawel.urbanski@gmail.com>
2019-06-08 16:23:30 +0200

    Add rgb control submit form.

--- web_app/flask_web_server.py
+++ web_app/flask_web_server.py
@@ -29,6 +29,21 @@ def index():
             client.connect("40.121.36.126", 1883)
             client.publish("control_1", "1")
             time.sleep(1)
+        elif request.form.get('change color') == 'change color':
+            client = mqtt.Client()
+            client.connect("40.121.36.126", 1883)
+            r = request.form['r']
+            client.publish("lamp_1", "r" + str(r))
+            time.sleep(0.1)
+            g = request.form['g']
+            client.publish("lamp_1", "g" + str(g))
+            time.sleep(0.1)
+            b = request.form['b']
+            client.publish("lamp_1", "b" + str(b))
+            time.sleep(0.1)
+            intensity = request.form['intensity']
+            client.publish("lamp_1", "i" + str(intensity))
+            time.sleep(1)
         else:
             # pass # unknown
             with open("/home/pawel2357/3d_printer.txt") as f:
@@ -49,5 +64,5 @@ def index():
     last_car = data[-1]
     return render_template("index.html", value=lastline[-150:], car_value=last_car[-150:])
     
-    if __name__ == '__main__':
-        app.run(host = '0.0.0.0')
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
