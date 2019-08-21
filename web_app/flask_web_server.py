8dd461ee87943c7a03b0a4202f245911306a76c1
Pawel2357 <2pawel.urbanski@gmail.com>
2019-08-21 08:42:20 +0200

    Polish code.

--- web_app/flask_web_server.py
+++ web_app/flask_web_server.py
@@ -2,19 +2,22 @@ from flask import Flask, render_template, request
 import paho.mqtt.client as mqtt
 import time
 
+broker_IP = "xxx"
+bathroom_topic = "lamp_1"
 
-def change_color(client, r, g, b, intensity):
+
+def change_color(client, r, g, b, intensity, topic):
     if r != '':
-        client.publish("lamp_1", "r" + str(r))
+        client.publish(topic, "r" + str(r))
     time.sleep(0.05)
     if g != '':
-        client.publish("lamp_1", "g" + str(g))
+        client.publish(topic, "g" + str(g))
     time.sleep(0.05)
     if b != '':
-        client.publish("lamp_1", "b" + str(b))
+        client.publish(topic, "b" + str(b))
     time.sleep(0.05)
     if intensity != '':
-        client.publish("lamp_1", "i" + str(intensity))
+        client.publish(topic, "i" + str(intensity))
 
 app = Flask(__name__)
 
@@ -25,75 +28,75 @@ def index():
     if request.method == 'POST':
         if request.form.get('Turn on') == 'Turn on':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
+            client.connect(broker_IP, 1883)
             client.publish("sensors_3d_printing_room", "0")
             time.sleep(1)
         elif  request.form.get('Turn off') == 'Turn off':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
+            client.connect(broker_IP, 1883)
             client.publish("sensors_3d_printing_room", "1")
             time.sleep(1)
         elif request.form.get('beast on') == 'beast on':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
+            client.connect(broker_IP, 1883)
             client.publish("control_1", "0")
             time.sleep(1)
         elif request.form.get('beast off') == 'beast off':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
+            client.connect(broker_IP, 1883)
             client.publish("control_1", "1")
             time.sleep(1)
         elif request.form.get('change color') == 'change color':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
+            client.connect(broker_IP, 1883)
             r = request.form['r']
             g = request.form['g']
             b = request.form['b']
             intensity = request.form['intensity']
-            change_color(client, r, g, b, intensity)
+            change_color(client, r, g, b, intensity, bathroom_topic)
         elif request.form.get('Off lamp') == 'Off lamp':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 255, 255, 255, 0)
+            client.connect(broker_IP, 1883)
+            change_color(client, 255, 255, 255, 0, bathroom_topic)
         elif request.form.get('White') == 'White':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 255, 255, 255, 255)
+            client.connect(broker_IP, 1883)
+            change_color(client, 255, 255, 255, 255, bathroom_topic)
         elif request.form.get('Blue') == 'Blue':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 0,191,255, 255)
+            client.connect(broker_IP, 1883)
+            change_color(client, 0, 191, 255, 255, bathroom_topic)
         elif request.form.get('Yellow') == 'Yellow':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 255,255,0, 255)
+            client.connect(broker_IP, 1883)
+            change_color(client, 255, 255, 0, 255, bathroom_topic)
         elif request.form.get('Green') == 'Green':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 50,205,50, 255)
+            client.connect(broker_IP, 1883)
+            change_color(client, 50, 205, 50, 255, bathroom_topic)
         elif request.form.get('Red') == 'Red':
             client = mqtt.Client()
-            client.connect("40.121.36.126", 1883)
-            change_color(client, 255,69,0, 255)
+            client.connect(broker_IP, 1883)
+            change_color(client, 255, 69, 0, 255, bathroom_topic)
         else:
             # pass # unknown
-            with open("/home/pawel2357/3d_printer.txt") as f:
-                data = f.readlines()
-            lastline = data[-1]
-            with open("/home/pawel2357/gps_data_correct.txt") as f:
-                data = f.readlines()
-            last_car = data[-1]
-            return render_template("index.html", value=lastline[-150:], car_value=last_car[-150:])
+#             with open("/home/pawel2357/3d_printer.txt") as f:
+#                 data = f.readlines()
+            lastline = "Cooming soon :)"
+#             with open("/home/pawel2357/gps_data_correct.txt") as f:
+#                 data = f.readlines()
+            last_car = "Cooming soon :)"
+            return render_template("index.html", value=lastline, car_value=last_car)
     elif request.method == 'GET':
         # return render_template("index.html")
         print("No Post Back Call")
-    with open("/home/pawel2357/3d_printer.txt") as f:
-        data = f.readlines()
-    lastline = data[-1]
-    with open("/home/pawel2357/gps_data_correct.txt") as f:
-        data = f.readlines()
-    last_car = data[-1]
-    return render_template("index.html", value=lastline[-150:], car_value=last_car[-150:])
+#     with open("/home/pawel2357/3d_printer.txt") as f:
+#         data = f.readlines()
+    lastline = "Cooming soon :)"
+#     with open("/home/pawel2357/gps_data_correct.txt") as f:
+#         data = f.readlines()
+    last_car = "Cooming soon :)"
+    return render_template("index.html", value=lastline, car_value=last_car)
     
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
