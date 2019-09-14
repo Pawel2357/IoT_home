ae3eeffce9019b0967c5f7d103f1977cdca85ac0
Pawel2357 <2pawel.urbanski@gmail.com>
2019-09-14 15:29:42 +0200

    Add automatic load switch.

--- renovable_energy_production/data_logger/subscribe_data.py
+++ renovable_energy_production/data_logger/subscribe_data.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+import csv
 
 # This is the Subscriber
 
@@ -7,22 +8,42 @@ filename = "battery_soc_data.txt"
 broker_ip = "xxx"
 soc_battery_topic = "home_battery_soc"
 
-def save_to_file(filename, message):
-     with open(filename, "a") as myfile:
-         myfile.write(message)
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code " + str(rc))
   client.subscribe(soc_battery_topic)
 
+
+def load_on(client):
+    client.publish("renovable_control", "6")
+    time.sleep(2)
+
+    client.publish("renovable_control", "7")
+    time.sleep(2)
+
+
+def load_off(client):
+    client.publish("renovable_control", "e")
+    time.sleep(2)
+    client.publish("renovable_control", "f")
+    time.sleep(2)
+
+
 def on_message(client, userdata, msg):
-  localtime = time.asctime(time.localtime(time.time()))
-  data = msg.topic + "," + str(msg.payload.decode("utf-8")) + "," + localtime
-  print(data)
-  save_to_file(filename, data)
+    localtime = time.asctime(time.localtime(time.time()))
+    with open('soc_data.csv', mode='wa') as file_soc:
+        soc = str(msg.payload.decode("utf-8"))
+
+        soc_data_writer = csv.writer(file_soc, delimiter=',')
+        soc_data_writer.writerow([localtime, soc])
+        if soc >= 90:
+            load_on(client)
+        if soc <= 75:
+            load_off(client)
+
 
 client = mqtt.Client()
-client.connect("40.121.36.126", 1883)
+client.connect(broker_ip, 1883)
 
 client.on_connect = on_connect
 client.on_message = on_message
