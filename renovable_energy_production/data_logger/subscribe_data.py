374044b33d508c59f1f00ed540c9c9ad1f70169a
Pawel2357 <2pawel.urbanski@gmail.com>
2019-09-15 13:34:07 +0200

    Debug

--- renovable_energy_production/data_logger/subscribe_data.py
+++ renovable_energy_production/data_logger/subscribe_data.py
@@ -15,31 +15,36 @@ def on_connect(client, userdata, flags, rc):
 
 
 def load_on(client):
-    client.publish("renovable_control", "6")
+    client_2 = mqtt.Client()
+    client_2.connect(broker_ip, 1883)
+    client_2.publish("renovable_control", "6")
+    time.sleep(2)
+    client_2.publish("renovable_control", "7")
     time.sleep(2)
 
-    client.publish("renovable_control", "7")
-    time.sleep(2)
 
-
-def load_off(client):
-    client.publish("renovable_control", "e")
-    time.sleep(2)
-    client.publish("renovable_control", "f")
-    time.sleep(2)
+def load_off():
+    client_2 = mqtt.Client()
+    client_2.connect(broker_ip, 1883)
+    client_2.publish("renovable_control", "e")
+    time.sleep(1)
+    client_2.publish("renovable_control", "f")
+    time.sleep(1)
 
 
 def on_message(client, userdata, msg):
     localtime = time.asctime(time.localtime(time.time()))
-    with open('soc_data.csv', mode='wa') as file_soc:
-        soc = int(str(msg.payload.decode("utf-8"))[1:-2])
+    soc = int(str(msg.payload.decode("utf-8"))[1:-1])
+    print(soc)
+    with open('soc_data.csv', mode='a') as file_soc:
+
 
         soc_data_writer = csv.writer(file_soc, delimiter=',')
         soc_data_writer.writerow([localtime, soc])
         if soc >= 90:
             load_on(client)
         if soc <= 75:
-            load_off(client)
+            load_off()
 
 
 client = mqtt.Client()
