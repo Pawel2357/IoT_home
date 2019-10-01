33bc6293f5914503262744337f6b0f1f1ad90b4c
Pawel2357 <2pawel.urbanski@gmail.com>
2019-10-01 21:23:14 +0200

    Send sensor logs to broker

--- climate_control/esp_climate_log.ino
+++ climate_control/esp_climate_log.ino
@@ -11,6 +11,30 @@
 //Include the library
 #include <MQUnifiedsensor.h>
 
+
+
+#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+
+// Connect to the WiFi
+const char* ssid =        "Dom_2_4";
+const char* password =    "izabelin";
+const char* mqtt_server = "192.168.1.198";
+char* MQTT_client =       "climate_log";
+char* humidity_t =        "humidity_climate";
+char* temp_t =            "temp_climate";
+char* co_t =              "co_climate";
+char* alcohol_t =         "alcohol_climate";
+char* co2_t =             "co2_climate";
+char* tolueno_t =         "tolueno_climate";
+char* nh4_t =             "nh4_climate";
+char* acetona_t =         "acetona_climate";
+
+
+
 #define DHTPIN 4     // what digital pin the DHT22 is conected to
 #define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
 
@@ -19,6 +43,8 @@
 #define type 135 //MQ135
 
 DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 //Declare Sensor
 MQUnifiedsensor MQ135(pin, type);
@@ -27,9 +53,62 @@ MQUnifiedsensor MQ135(pin, type);
 float CO, Alcohol, CO2, Tolueno, NH4, Acetona;
 
 
+void setup_wifi() {
+
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+   Serial.print("Attempting MQTT connection...");
+   yield();
+   // Attempt to connect
+ if (client.connect(MQTT_client)) {
+   Serial.println("connected");
+   // ... and subscribe to topic
+   // client.subscribe("lamp_1");
+ } else {
+   Serial.print("failed, rc=");
+   Serial.print(client.state());
+   Serial.println(" try again in 5 seconds");
+   // Wait 5 seconds before retrying
+   delay(5000);
+   }
+ }
+}
+
+
+void publish_data(char* topic, String measure)
+{
+  client.publish(topic, (char*) measure.c_str());
+}
+
+
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(2000);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
 
   // Wait for serial to initialize.
   while(!Serial) { }
@@ -42,11 +121,17 @@ void setup() {
 
 }
 
+
 int timeSinceLastRead = 0;
 void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   
   // Report every 2 seconds.
-  if(timeSinceLastRead > 2000) {
+  if(timeSinceLastRead > 20000) {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h = dht.readHumidity();
@@ -70,43 +155,46 @@ void loop() {
 
     Serial.print("Humidity: ");
     Serial.print(h);
+    publish_data(humidity_t, String(h));
     Serial.print(" %\t");
     Serial.print("Temperature: ");
     Serial.print(t);
+    publish_data(temp_t, String(t));
     Serial.print(" *C ");
-    Serial.print(f);
-    Serial.print(" *F\t");
-    Serial.print("Heat index: ");
-    Serial.print(hic);
-    Serial.print(" *C ");
-    Serial.print(hif);
-    Serial.println(" *F");
+    
+
+
+    MQ135.update();
+
+    CO =  MQ135.readSensor("CO"); // Return CO concentration
+    Alcohol =  MQ135.readSensor("Alcohol"); // Return Alcohol concentration
+    CO2 =  MQ135.readSensor("CO2"); // Return CO2 concentration
+    Tolueno =  MQ135.readSensor("Tolueno"); // Return Tolueno concentration
+    NH4 =  MQ135.readSensor("NH4"); // Return NH4 concentration
+    Acetona =  MQ135.readSensor("Acetona"); // Return Acetona concentration
+
+    Serial.println("***************************");
+    Serial.println("Lectures for MQ-135");
+    Serial.print("Volt: ");Serial.print(MQ135.getVoltage(false));Serial.println(" V"); 
+    Serial.print("R0: ");Serial.print(MQ135.getR0());Serial.println(" Ohm"); 
+    Serial.print("CO: ");Serial.print(CO,2);Serial.println(" ppm");
+    publish_data(co_t, String(CO));
+    Serial.print("Alcohol: ");Serial.print(Alcohol,2);Serial.println(" ppm");
+    publish_data(alcohol_t, String(Alcohol));
+    Serial.print("CO2: ");Serial.print(CO2,2);Serial.println(" ppm");
+    publish_data(co2_t, String(CO2));
+    Serial.print("Tolueno: ");Serial.print(Tolueno,2);Serial.println(" ppm");
+    publish_data(tolueno_t, String(Tolueno));
+    Serial.print("NH4: ");Serial.print(NH4,2);Serial.println(" ppm");
+    publish_data(nh4_t, String(NH4));
+    Serial.print("Acetona: ");Serial.print(Acetona,2);Serial.println(" ppm");
+    publish_data(acetona_t, String(Acetona));
+    Serial.println("***************************"); 
+    
 
     timeSinceLastRead = 0;
   }
   delay(100);
-  timeSinceLastRead += 100;
-
-  MQ135.update();
-
-  CO =  MQ135.readSensor("CO"); // Return CO concentration
-  Alcohol =  MQ135.readSensor("Alcohol"); // Return Alcohol concentration
-  CO2 =  MQ135.readSensor("CO2"); // Return CO2 concentration
-  Tolueno =  MQ135.readSensor("Tolueno"); // Return Tolueno concentration
-  NH4 =  MQ135.readSensor("NH4"); // Return NH4 concentration
-  Acetona =  MQ135.readSensor("Acetona"); // Return Acetona concentration
-
-
-  Serial.println("***************************");
-  Serial.println("Lectures for MQ-135");
-  Serial.print("Volt: ");Serial.print(MQ135.getVoltage(false));Serial.println(" V"); 
-  Serial.print("R0: ");Serial.print(MQ135.getR0());Serial.println(" Ohm"); 
-  Serial.print("CO: ");Serial.print(CO,2);Serial.println(" ppm");
-  Serial.print("Alcohol: ");Serial.print(Alcohol,2);Serial.println(" ppm");
-  Serial.print("CO2: ");Serial.print(CO2,2);Serial.println(" ppm");
-  Serial.print("Tolueno: ");Serial.print(Tolueno,2);Serial.println(" ppm");
-  Serial.print("NH4: ");Serial.print(NH4,2);Serial.println(" ppm");
-  Serial.print("Acetona: ");Serial.print(Acetona,2);Serial.println(" ppm");
-  Serial.println("***************************");  
+  timeSinceLastRead += 100; 
   
 }
