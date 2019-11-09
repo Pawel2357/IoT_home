905867183f482b2c7f40ad5b3c85a8ddc32db32c
Pawel2357 <2pawel.urbanski@gmail.com>
2019-11-09 09:53:42 +0100

    Debug

--- energy_monitor/save_energy_data.py
+++ energy_monitor/save_energy_data.py
@@ -7,21 +7,21 @@ import numpy as np
 
 filename = 'power_7_data.csv'
 broker_ip = "0.0.0.0"
-soc_battery_topic = "energy_monitor"
+power_topic = "energy_monitor_0"
 
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code " + str(rc))
-  client.subscribe(soc_battery_topic)
+  client.subscribe(power_topic)
 
 
 def on_message(client, userdata, msg):
     localtime = time.asctime(time.localtime(time.time()))
     power = str(msg.payload.decode("utf-8"))
     print(power)
-    with open(filename, mode='a') as file_soc:
-        soc_data_writer = csv.writer(file_soc, delimiter=',')
-        soc_data_writer.writerow([localtime, power])
+    with open(filename, mode='a') as file_power:
+        data_writer = csv.writer(file_power, delimiter=',')
+        data_writer.writerow([localtime, power])
 
 client = mqtt.Client()
 client.connect(broker_ip, 1883)
