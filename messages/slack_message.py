fcd1507a1874ebddad4e2ec95cda4c9239435422
Pawel2357 <2pawel.urbanski@gmail.com>
2019-12-23 10:46:16 +0100

    Add battery load 6 notification.

--- messages/slack_message.py
+++ messages/slack_message.py
@@ -1,11 +1,12 @@
 import os
 import time
+import datetime
 import numpy as np
 import slack
 
-client = slack.WebClient(token='iii')
-climate_data_file = 'path'
-soc_data_file = 'path'
+client = slack.WebClient(token='xoxp-837260216032-824471975746-839473999942-2f3f9c7be1d876e8844cec9a79994bd6')
+climate_data_file = '/home/pawel/Documents/IoT_home/data/climate_data.csv'
+soc_data_file = '/home/pawel/Documents/IoT_home/data/soc_data.csv'
 
 low_temp = False
 high_temp = False
@@ -13,13 +14,19 @@ SoC_high = False
 SoC_low = False
 humid_low = False
 humid_high = False
+cable_6_on = False
+cable_6_off = False
+
 
 temp_min_v = 15
 temp_max_v = 22
-soc_low_v = 35
+soc_low_v = 41
 soc_high_v = 90
 humid_low_v = 25
 humid_high_v = 60
+soc_6_off_v = 40
+soc_6_on_v = 46
+
 
 def send_message(message, channel='#smart-home'):
     response = client.chat_postMessage(
@@ -57,17 +64,31 @@ def logic():
 while True:
     soc = read_soc(soc_data_file)
     temp, humidity = read_temperature(climate_data_file)
-    time.sleep(5)
+    time.sleep(20)
+    now = datetime.datetime.now()
+    if now.minute == 0:
+        low_temp = False
+        high_temp = False
+        SoC_high = False
+        SoC_low = False
+        humid_low = False
+        humid_high = False
     if soc < soc_low_v and not SoC_low:
         SoC_low, SoC_high = logic()
         send_message("Low battery charge: " + str(soc))
     if soc > soc_high_v and not SoC_high:
         SoC_high, SoC_low = logic()
         send_message("High battery charge: " + str(soc))
+    if soc < soc_6_off_v and not cable_6_off:
+        cable_6_off, cable_6_on = logic()
+        send_message("Wire 6 battery off: " + str(soc))
+    if soc > soc_6_on_v and not cable_6_on:
+        cable_6_on, cable_6_off = logic()
+        send_message("Wire 6 battery on: " + str(soc))
     if temp < temp_min_v and not low_temp:
         low_temp, high_temp = logic()
         send_message("Low temperature: " + str(soc))
-    if temp > soc_high_v and not high_temp:
+    if temp > temp_max_v and not high_temp:
         high_temp, low_temp = logic()
         send_message("High temperature: " + str(soc))
     if humidity < humid_low_v and not humid_low:
