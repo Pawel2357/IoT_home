1d2503912cc81b77669edf19d6c000a08e46fcb8
Pawel2357 <2pawel.urbanski@gmail.com>
2019-12-30 21:04:15 +0100

    Correct code. Send signals using I/O pins.

--- smart_charger/arduino.ino
+++ smart_charger/arduino.ino
@@ -1,6 +1,6 @@
-String a; //Initialized variable to store recieved data
 const int pin_1 = 10;
 const int pin_2 = 11;
+const int NUM_SAMPLES = 15;
 
 void setup() {
   //Serial Begin at 9600 Baud
@@ -11,23 +11,37 @@ void setup() {
   digitalWrite(pin_2, HIGH);
 }
 
-void loop() {
 
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    a = Serial.readString();
-
-    // TODO: iterate over elements in this string or look at nodemcu
-
-    // say what you got:
-    if (a == "1"){
-      digitalWrite(pin_1, LOW); // turn on
-    }if (a == "0"){
-      digitalWrite(pin_1, HIGH); // turn off
-    }if (a == "3"){
-      digitalWrite(pin_2, LOW); // high speed
-    }if (a == "2"){
-      digitalWrite(pin_2, HIGH); // low speed
+float measure_voltage(int pin){
+    // take a number of analog samples and add them up
+    int sample_count = 0;
+    int sum = 0;
+    while (sample_count < NUM_SAMPLES) {
+        sum += analogRead(pin);
+        sample_count++;
+        delay(10);
     }
+    // calculate the voltage
+    // use 5.0 for a 5.0V ADC reference voltage
+    // 5.015V is the calibrated reference voltage
+    float voltage = ((float)sum / (float)NUM_SAMPLES * 5) / 1024.0;
+    // send voltage for display on Serial Monitor
+    // voltage multiplied by 11 when using voltage divider that
+    // divides by 11. 11.132 is the calibrated voltage divide
+    // value
+    return voltage;
+}
+
+void loop() {
+  //digitalWrite(pin_1, HIGH); // turn off
+  //delay(1000);
+  //digitalWrite(pin_2, LOW); // high speed
+
+  float voltage_A0 = measure_voltage(A0);
+
+  if(voltage_A0 > 2.5){
+    digitalWrite(pin_1, LOW); // turn on
+  }else{
+    digitalWrite(pin_1, HIGH); // turn off
   }
 }
