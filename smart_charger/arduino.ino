453019d9a37efa6d4529c47899b5e59e584219a5
Pawel2357 <2pawel.urbanski@gmail.com>
2019-12-31 11:40:09 +0100

    Improve code. Control multiple relays.

--- smart_charger/arduino.ino
+++ smart_charger/arduino.ino
@@ -1,14 +1,25 @@
-const int pin_1 = 10;
-const int pin_2 = 11;
+const int pin_0 = 10;
+const int pin_1 = 11;
+const int pin_2 = 12;
+const int pin_3 = 13;
+const int pin_in_0 = A0;
+const int pin_in_1 = A1;
+const int pin_in_2 = A2;
+const int pin_in_3 = A3;
 const int NUM_SAMPLES = 15;
+const float VOLTAGE_THR = 2.5;
 
 void setup() {
   //Serial Begin at 9600 Baud
-  pinMode(pin_1, OUTPUT);
+  pinMode(pin_0, OUTPUT);
   Serial.begin(9600);
+  digitalWrite(pin_0, HIGH);
+  pinMode(pin_1, OUTPUT);
   digitalWrite(pin_1, HIGH);
   pinMode(pin_2, OUTPUT);
   digitalWrite(pin_2, HIGH);
+  pinMode(pin_3, OUTPUT);
+  digitalWrite(pin_3, HIGH);
 }
 
 
@@ -32,16 +43,21 @@ float measure_voltage(int pin){
     return voltage;
 }
 
+void set_pin_value(int pin_in, int pin){
+  float voltage = measure_voltage(pin_in);
+  if(voltage > VOLTAGE_THR){
+    digitalWrite(pin, LOW); // turn on
+  }else{
+    digitalWrite(pin, HIGH); // turn off
+  }
+}
+
 void loop() {
   //digitalWrite(pin_1, HIGH); // turn off
   //delay(1000);
   //digitalWrite(pin_2, LOW); // high speed
-
-  float voltage_A0 = measure_voltage(A0);
-
-  if(voltage_A0 > 2.5){
-    digitalWrite(pin_1, LOW); // turn on
-  }else{
-    digitalWrite(pin_1, HIGH); // turn off
-  }
+  set_pin_value(int pin_in_0, int pin_0);
+  set_pin_value(int pin_in_1, int pin_1);
+  set_pin_value(int pin_in_2, int pin_2);
+  set_pin_value(int pin_in_3, int pin_3);
 }
