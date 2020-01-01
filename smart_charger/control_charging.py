ed5a6bc52853f51686816f48641c178df139cb64
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-01 15:52:32 +0100

    Correct auto turn off

--- smart_charger/control_charging.py
+++ smart_charger/control_charging.py
@@ -1,13 +1,17 @@
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import time
 import csv
+import datetime
 import numpy as np
 
 # This is the Subscriber
 
-broker_ip = "0.0.0.0"
+broker_ip = "zzz"
 topic = "charging_1"
 charging_time = 20
+
+now = datetime.datetime.now()
 
 
 def on_connect(client, userdata, flags, rc):
@@ -18,14 +22,12 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     localtime = time.asctime(time.localtime(time.time()))
     command = str(msg.payload.decode("utf-8"))
-    print(command)
-    if command == '4':
+    print(int(command))
+    if int(command) == 4:
         time.sleep(charging_time * 60)
-        client = mqtt.Client()
-        client.connect(broker_IP, 1883)
-        client.publish(topic, '0')
+        publish.single(topic, "0", hostname=broker_ip)
 
-client = mqtt.Client()
+client = mqtt.Client("test_1")
 client.connect(broker_ip, 1883)
 
 client.on_connect = on_connect
