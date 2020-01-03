0fee6d7697dcfe59ff8a439fc6ed01075e37b104
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-03 09:22:08 +0100

    Log temperature to file.

---
+++ climate_control/floor_heating/temperature_kitchen_floor_log.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import time
+import csv
+
+# This is the Subscriber
+
+filename = "/home/pawel/Documents/IoT_home/data/kitchen_floor_temp_data.csv"
+broker_ip = "192.168.1.198"
+soc_battery_topic = "kitchen_floor_temp"
+
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code " + str(rc))
+  client.subscribe(soc_battery_topic)
+
+
+
+def on_message(client, userdata, msg):
+    localtime = time.asctime(time.localtime(time.time()))
+    data = str(msg.payload.decode("utf-8"))
+    with open(filename, mode='a') as file_soc:
+
+
+        soc_data_writer = csv.writer(file_soc, delimiter=',')
+        soc_data_writer.writerow([localtime, data])
+
+
+client = mqtt.Client()
+client.connect(broker_ip, 1883)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
