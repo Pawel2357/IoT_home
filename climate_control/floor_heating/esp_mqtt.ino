edf33bb2293d313a8f45d7644d39af2fbe38a20f
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-03 08:43:41 +0100

    Add floor temperature measurement.

--- climate_control/floor_heating/esp_mqtt.ino
+++ climate_control/floor_heating/esp_mqtt.ino
@@ -10,12 +10,47 @@ const char* mqtt_server = "xyz";
 
 const char* mqtt_client_name = "kitchen_floor_heating";
 const char* topic_subscribe = "kitchen_floor_heating";
+const char* topic_kitchen_floor_temp = "kitchen_floor_temp";
+
+#define THERMISTORPIN A0         
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000    
+
+uint8_t pin_0 = D0;
+uint8_t pin_1 = D1;
+uint8_t pin_2 = D2;
+uint8_t pin_3 = D3;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void send_to_arduino(char receivedChar){
-  Serial.write(receivedChar);
+  if(receivedChar == '0'){
+    digitalWrite(pin_0, LOW);
+  }if(receivedChar == '1'){
+    digitalWrite(pin_1, LOW);
+  }if(receivedChar == '2'){
+    digitalWrite(pin_2, LOW);
+  }if(receivedChar == '3'){
+    digitalWrite(pin_3, LOW);
+  }if(receivedChar == '4'){
+    digitalWrite(pin_0, HIGH);
+  }if(receivedChar == '5'){
+    digitalWrite(pin_1, HIGH);
+  }if(receivedChar == '6'){
+    digitalWrite(pin_2, HIGH);
+  }if(receivedChar == '7'){
+    digitalWrite(pin_3, HIGH);
+  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -60,19 +95,71 @@ void reconnect() {
  }
 }
 
+float measure_voltage() {
+  uint8_t i;
+  float average;
+  int samples[NUMSAMPLES];
+ 
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(THERMISTORPIN);
+   delay(10);
+  }
+  
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+ 
+  Serial.print("Average analog reading "); 
+  Serial.println(average);
+  
+  // convert the value to resistance
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+  Serial.print("Thermistor resistance "); 
+  Serial.println(average);
+  
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+  
+  Serial.print("Temperature "); 
+  Serial.print(steinhart);
+  Serial.println(" *C");
+  
+  delay(1000);
+  return steinhart;
+}
+
 void setup()
 {
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  pinMode(pin_0, OUTPUT);
+  digitalWrite(pin_0, LOW);
+  pinMode(pin_1, OUTPUT);
+  digitalWrite(pin_1, LOW);
+  pinMode(pin_2, OUTPUT);
+  digitalWrite(pin_2, LOW);
+  pinMode(pin_3, OUTPUT);
+  digitalWrite(pin_3, LOW);
 }
 
 
 void loop()
 {
   if (!client.connected()) {
-  reconnect();
+    reconnect();
   }
   client.loop();
+  client.publish(topic_kitchen_floor_temp, String(measure_voltage());
 }
