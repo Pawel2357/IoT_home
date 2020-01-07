9b6cd101e80c2d2ba13bfe16ab1e4b6e205904ab
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-07 11:45:55 +0100

    Simplify by implementing notification class.

--- messages/slack_message.py
+++ messages/slack_message.py
@@ -4,21 +4,12 @@ import datetime
 import numpy as np
 import slack
 
-client = slack.WebClient(token='xoxp-837260216032-824471975746-839473999942-2f3f9c7be1d876e8844cec9a79994bd6')
+client = slack.WebClient(token='xoxp-837260216032-824471975746-887198095136-2852144328356a6de80d22463cbef2ed')
 climate_data_file = '/home/pawel/Documents/IoT_home/data/climate_data.csv'
 soc_data_file = '/home/pawel/Documents/IoT_home/data/soc_data.csv'
+climate_data_stairs_filename = '/home/pawel/Documents/IoT_home/data/stairs_climate_data.csv'
 
-low_temp = False
-high_temp = False
-SoC_high = False
-SoC_low = False
-humid_low = False
-humid_high = False
-cable_6_on = False
-cable_6_off = False
-
-
-temp_min_v = 15
+temp_min_v = 19.5
 temp_max_v = 22
 soc_low_v = 41
 soc_high_v = 90
@@ -26,14 +17,38 @@ humid_low_v = 25
 humid_high_v = 60
 soc_6_off_v = 40
 soc_6_on_v = 46
+condensation_s_humid_v = 62
+condensation_s_temp_v = 13
 
-
-def send_message(message, channel='#smart-home'):
-    response = client.chat_postMessage(
-        channel=channel,
-        text=message)
-    assert response["ok"]
-    assert response["message"]["text"] == message
+class Notification:
+    def __init__(self, notification_delay, channel='#smart-home'):
+        self.last_notification_time = None
+        self.notification_delay = notification_delay
+        self.channel = channel
+    
+    def can_notify(self):
+        if self.last_notification_time is not None:
+            now = datetime.datetime.now()
+            d_t = now - self.last_notification_time
+            if d_t.seconds / 60 > self.notification_delay:
+                return True
+            else:
+                return False
+        else:
+            self.last_notification_time = datetime.datetime.now()
+            return True
+    def reset(self):
+        self.last_notification_time = None
+        
+    def send_message(self, message):
+        response = client.chat_postMessage(
+            channel=self.channel,
+            text=message)
+        assert response["ok"]
+        assert response["message"]["text"] == message
+            
+    def notify(self, message):
+        self.send_message(message)
     
 def read_temperature(data_file):
     with open(data_file) as csvfile:
@@ -58,42 +73,64 @@ def read_soc(filename_soc):
             soc_l.append(int(soc[1]))
     return np.mean(soc_l)
 
-def logic():
-    return True, False
+def read_temp_humid_stairs(data_filename):
+    with open(data_filename) as csvfile:
+        last10 = list(csvfile)[-100:]
+    temp = []
+    humidity = []
+    for row in last10:
+        x = row.split(",")
+        temp.append(float(x[2][:-2]))
+        humidity.append(float(x[1][1:]))
+
+    humidity_correct = []
+    temp_correct = []
+    for i, h in enumerate(humidity):
+        if h <= 100:
+            humidity_correct.append(h)
+            temp_correct.append(temp[i])
+    return np.mean(temp_correct[-3:]), np.mean(humidity_correct[-3:])
+
+low_temp = Notification(60)
+high_temp = Notification(60)
+SoC_high = Notification(60)
+SoC_low = Notification(60)
+humid_low = Notification(60)
+humid_high = Notification(60)
+condensation_stairs_warning = Notification(60)
+cable_6_on = Notification(360)
+cable_6_off = Notification(360)
 
 while True:
     soc = read_soc(soc_data_file)
     temp, humidity = read_temperature(climate_data_file)
+    temp_s, humidity_s = read_temp_humid_stairs(climate_data_stairs_filename)
     time.sleep(20)
-    now = datetime.datetime.now()
-    if now.minute == 0:
-        low_temp = False
-        high_temp = False
-        SoC_high = False
-        SoC_low = False
-        humid_low = False
-        humid_high = False
-    if soc < soc_low_v and not SoC_low:
-        SoC_low, SoC_high = logic()
-        send_message("Low battery charge: " + str(soc))
-    if soc > soc_high_v and not SoC_high:
-        SoC_high, SoC_low = logic()
-        send_message("High battery charge: " + str(soc))
-    if soc < soc_6_off_v and not cable_6_off:
-        cable_6_off, cable_6_on = logic()
-        send_message("Wire 6 battery off: " + str(soc))
-    if soc > soc_6_on_v and not cable_6_on:
-        cable_6_on, cable_6_off = logic()
-        send_message("Wire 6 battery on: " + str(soc))
-    if temp < temp_min_v and not low_temp:
-        low_temp, high_temp = logic()
-        send_message("Low temperature: " + str(temp))
-    if temp > temp_max_v and not high_temp:
-        high_temp, low_temp = logic()
-        send_message("High temperature: " + str(temp))
-    if humidity < humid_low_v and not humid_low:
-        humid_low, humid_high = logic()
-        send_message("Low humidity: " + str(humidity))
-    if humidity > humid_high_v and not humid_high:
-        humid_high, humid_low = logic()
-        send_message("High humidity: " + str(humidity))
+    if soc < soc_low_v and SoC_low.can_notify():
+        SoC_low.notify("Low battery charge: " + str(soc))
+        SoC_high.reset()
+    if soc > soc_high_v and not SoC_high.can_notify():
+        SoC_high.notify("High battery charge: " + str(soc))
+        SoC_low.reset()
+    if soc < soc_6_off_v and cable_6_off.can_notify():
+        cable_6_off.notify("Wire 6 battery off: " + str(soc))
+        cable_6_on.reset()
+    if soc > soc_6_on_v and cable_6_on.can_notify():
+        cable_6_on.notify("Wire 6 battery on: " + str(soc))
+        cable_6_off.reset()
+    if temp < temp_min_v and low_temp.can_notify():
+        low_temp.notify("Low temperature: " + str(temp))
+        high_temp.reset()
+    if temp > temp_max_v and high_temp.can_notify():
+        high_temp.notify("High temperature: " + str(temp))
+        low_temp.reset()
+    if humidity < humid_low_v and not humid_low.can_notify():
+        humid_low.notify("Low humidity: " + str(humidity))
+        humid_high.reset()
+    if humidity > humid_high_v and not humid_high.can_notify():
+        humid_high.notify("High humidity: " + str(humidity))
+        humid_low.reset()
+    if humidity_s > condensation_s_humid_v and condensation_stairs_warning.can_notify():
+        condensation_stairs_warning.notify("Wall condensation, humid: " + str(humidity_s))
+    if condensation_s_temp_v < temp_s and humidity_s > condensation_s_humid_v - 5 and condensation_stairs_warning.can_notify():
+        condensation_stairs_warning.notify("Wall condensation, humid: " + str(humidity_s) + " temp: " + str(temp_s))
