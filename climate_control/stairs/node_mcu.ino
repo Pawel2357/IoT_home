d622edf8970bd6aa1e5ff2418e2c725e508793fe
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-07 13:04:42 +0100

    Add option to turn on dehumidifier and ventilation.

--- climate_control/stairs/node_mcu.ino
+++ climate_control/stairs/node_mcu.ino
@@ -19,6 +19,32 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+uint8_t pin_0 = D0;
+uint8_t pin_1 = D1;
+uint8_t pin_2 = D2;
+uint8_t pin_3 = D3;
+
+void send_to_arduino(char receivedChar){
+  Serial.print(receivedChar);
+  if(receivedChar == '0'){
+    digitalWrite(pin_0, LOW);
+  }if(receivedChar == '1'){
+    digitalWrite(pin_1, LOW);
+  }if(receivedChar == '2'){
+    digitalWrite(pin_2, LOW);
+  }if(receivedChar == '3'){
+    digitalWrite(pin_3, LOW);
+  }if(receivedChar == '4'){
+    digitalWrite(pin_0, HIGH);
+  }if(receivedChar == '5'){
+    digitalWrite(pin_1, HIGH);
+  }if(receivedChar == '6'){
+    digitalWrite(pin_2, HIGH);
+  }if(receivedChar == '7'){
+    digitalWrite(pin_3, HIGH);
+  }
+}
+
 void setup_wifi() {
     delay(10);
     // We start by connecting to a WiFi network
@@ -60,9 +86,36 @@ void reconnect() {
  }
 }
 
-void publish_data(char* topic, String measure)
-{
+void turn_off_all(){
+  digitalWrite(pin_0, LOW);
+  digitalWrite(pin_1, LOW);
+  digitalWrite(pin_2, LOW);
+  digitalWrite(pin_3, LOW);
+}
+
+void publish_data(char* topic, String measure){
   client.publish(topic, (char*) measure.c_str());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ // get subscribed message char by char
+ // TODO: Do it separately for different topics or add time distance between got chars to communicate separately.
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  if(receivedChar == 't'){
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      timeSinceLastRead = 0;
+      return;
+    }else{
+      publish_data(climate_topic, String(h) + "," + String(t));
+    }
+  }else{
+    send_to_arduino(receivedChar);
+  }
+ }
 }
 
 void setup(){
@@ -71,41 +124,21 @@ void setup(){
 
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(pin_0, OUTPUT);
+  pinMode(pin_1, OUTPUT);
+  pinMode(pin_2, OUTPUT);
+  pinMode(pin_3, OUTPUT);
+  turn_off_all();
 
   // Wait for serial to initialize.
   while(!Serial) { }
 }
 
-int timeSinceLastRead = 0;
-
 void loop(){
-
   if (!client.connected()) {
+    turn_off_all();
     reconnect();
   }
   client.loop();
-
-    // Report every 2 seconds.
-  if(timeSinceLastRead > 20000) {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    delay(3000); 
-    Serial.println(String(h) + ' ' + String(t));          
-
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
-      Serial.println("Failed to read from DHT sensor!");
-      timeSinceLastRead = 0;
-      return;
-    }
-                        // wait 100ms for next reading
-
-    publish_data(climate_topic, String(h) + "," + String(t));
-    timeSinceLastRead = 0;
-  }
-  delay(100);
-  timeSinceLastRead += 100; 
 }
