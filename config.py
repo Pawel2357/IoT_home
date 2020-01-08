7208b70a680b99f9d0efbf8ec1f5a538429d8887
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-08 13:40:37 +0100

    Add general config file.

---
+++ config.py
@@ -0,0 +1,33 @@
+# MQTT
+broker_ip = "xyz"
+broker_port = xyz
+
+topic_ventilation = "ventilation_living_room"
+# lamps
+topic_lamp_enterance = "lamp_enterance"
+# other
+kitchen_floor_heating_topic = "kitchen_floor_heating"
+topic = "charging_1"
+topic_stairs_climate = "climate_stairs"
+
+
+climate_data_file = '/home/pawel/Documents/IoT_home/data/climate_data.csv'
+soc_data_file = '/home/pawel/Documents/IoT_home/data/soc_data.csv'
+climate_data_stairs_filename = '/home/pawel/Documents/IoT_home/data/stairs_climate_data.csv'
+floor_temp_data = '/home/pawel/Documents/IoT_home/data/kitchen_floor_temp_data.csv'
+
+# Charger
+charging_time = 20
+
+# Notifications
+token='xoxp-837260216032-824471975746-883270421618-935044e3cba7f9b4e673199ddae7de71'
+temp_min_v = 19.5
+temp_max_v = 22
+soc_low_v = 41
+soc_high_v = 90
+humid_low_v = 25
+humid_high_v = 60
+soc_6_off_v = 40
+soc_6_on_v = 46
+condensation_s_humid_v = 62
+condensation_s_temp_v = 13
