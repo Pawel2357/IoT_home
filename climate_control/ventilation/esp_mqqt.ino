04bbdde74736ada52618b55af003b6ca0037900f
Pawel2357 <2pawel.urbanski@gmail.com>
2020-01-27 20:38:14 +0100

    Use ssr to control ventilation-recuperation.

--- climate_control/ventilation/esp_mqqt.ino
+++ climate_control/ventilation/esp_mqqt.ino
@@ -4,8 +4,8 @@
 #include <Wire.h>
 
 // Connect to the WiFi
-const char* ssid = "Dom_2_4";
-const char* password = "izabelin";
+const char* ssid = "xyz";
+const char* password = "xyz";
 const char* mqtt_server = "xyz";
 
 const char* mqtt_client_name = "ventilation_living_room";
@@ -14,8 +14,24 @@ const char* topic_subscribe = "ventilation_living_room";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+uint8_t pin_0 = D1;
+uint8_t pin_1 = D2;
+uint8_t pin_2 = D3;
+
 void send_to_arduino(char receivedChar){
-  Serial.write(receivedChar);
+  if(receivedChar == '0'){
+    digitalWrite(pin_0, LOW);
+  }if(receivedChar == '1'){
+    digitalWrite(pin_1, LOW);
+  }if(receivedChar == '2'){
+    digitalWrite(pin_2, LOW);
+  }if(receivedChar == '4'){
+    digitalWrite(pin_0, HIGH);
+  }if(receivedChar == '5'){
+    digitalWrite(pin_1, HIGH);
+  }if(receivedChar == '6'){
+    digitalWrite(pin_2, HIGH);
+  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -66,6 +82,12 @@ void setup()
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  pinMode(pin_0, OUTPUT);
+  digitalWrite(pin_0, LOW);
+  pinMode(pin_1, OUTPUT);
+  digitalWrite(pin_1, LOW);
+  pinMode(pin_2, OUTPUT);
+  digitalWrite(pin_2, LOW);
 }
 
 
