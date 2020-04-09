9301b63cef4e0f536b29f4c901a3cf4fbc01a7b3
Pawel2357 <2pawel.urbanski@gmail.com>
2020-04-09 20:23:38 +0200

    Measure heat pump power.

--- energy_monitor/arduino.ino
+++ energy_monitor/arduino.ino
@@ -2,17 +2,22 @@
 
 #include "EmonLib.h"                   // Include Emon Library
 EnergyMonitor emon1;                   // Create an instance
+EnergyMonitor emon2;                   // Create an instance
 
 void setup()
 {  
   Serial.begin(9600);
   
   emon1.current(2, 22);             // Current: input pin, calibration.
+  emon2.current(3, 22);             // Current: input pin, calibration.
 }
 
 void loop()
 {
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-  Serial.print(String(Irms*230.0) + ",Power_7");         // Apparent power
+  double Irms_hp = emon2.calcIrms(1480);  // Calculate Irms only
+  Serial.print(String(Irms * 230.0) + ",Power_7,");         // Apparent power
+  delay(500);
+  Serial.print(String(Irms_hp * 230.0) + ",Power_hp");         // Apparent power
   delay(5000);
 }
