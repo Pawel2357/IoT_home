8ee8c1766e1c7d2bdb706559f004d1334271350d
Pawel2357 <2pawel.urbanski@gmail.com>
2021-01-16 21:52:38 +0100

    Add two pir support. Add mqtt.

--- smart_light/pir_stairs_improved.ino
+++ smart_light/pir_stairs_improved.ino
@@ -1,16 +1,14 @@
-#include "DHTesp.h"
-
 #include <EEPROM.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
 
 // Connect to the WiFi
-const char* ssid =        "xyz";
-const char* password =    "xyz";
-const char* mqtt_server = "xyz";
-char* MQTT_client =       "pir_stairs";
-char* climate_topic =     "pir_stairs";
+const char* ssid =         "xyz";
+const char* password =     "xyz";
+const char* mqtt_server =  "xyz";
+char* MQTT_client =        "pir_stairs";
+char* lamp_control_topic = "lamp_stairs_control";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -24,6 +22,8 @@ int pirState2 = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
 unsigned long lastStatusLight1 = 0;
 unsigned long lastStatusLight2 = 0;
+unsigned long lastStatusWifi = 0;
+unsigned long lastStatusMqtt = 0;
 char* pir_1_topic =     "pir_stairs_1";
 char* pir_2_topic =     "pir_stairs_2";
 
@@ -34,36 +34,35 @@ void setup_wifi() {
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    if (millis() - lastStatusWifi > 500){
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      lastStatusWifi = millis();
+  
+      if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+      }
     }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
   // Loop until we're reconnected
-  while (!client.connected()) {
+  if ((!client.connected()) && (millis() - lastStatusMqtt > 5000)) {
     Serial.print("Attempting MQTT connection...");
     yield();
+    lastStatusMqtt = millis();
     // Attempt to connect
     if (client.connect(MQTT_client)) {
       Serial.println("connected");
       // ... and subscribe to topic
-      // client.subscribe("lamp_1");
+      client.subscribe(lamp_control_topic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
     }
   }
 }
@@ -77,12 +76,12 @@ void notify_pir_1(){
   val = digitalRead(inputPin);  // read input value
   if (val == HIGH){            // check if the input is HIGH
     if (pirState == LOW) {
-      publish_data(pir_1_topic, String("motion_start"));
+      publish_data(pir_1_topic, String("motion_start_1"));
       pirState = HIGH;
     }
   else{
     if (pirState == HIGH) {
-      publish_data(pir_1_topic, String("motion_end"));
+      publish_data(pir_1_topic, String("motion_end_1"));
       pirState == LOW;
       }
     }
@@ -93,12 +92,12 @@ void notify_pir_2(){
   val2 = digitalRead(inputPin2);  // read input value
   if (val2 == HIGH){            // check if the input is HIGH
     if (pirState2 == LOW) {
-      publish_data(pir_2_topic, String("motion_start"));
+      publish_data(pir_2_topic, String("motion_start_2"));
       pirState2 = HIGH;
     }
   else{
     if (pirState2 == HIGH) {
-      publish_data(pir_2_topic, String("motion_end"));
+      publish_data(pir_2_topic, String("motion_end_2"));
       pirState2 == LOW;
       }
     }
@@ -130,6 +129,23 @@ void control_lamp_1(){
       }
     }
   }
+
+void control_lamp_mqtt(char receivedChar){
+  Serial.print(receivedChar);
+  if(receivedChar=="1"){
+    digitalWrite(lampPin, LOW); // turn lamp on
+    lastStatusLight1 = millis();
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ // get subscribed message char by char
+ // TODO: Do it separately for different topics or add time distance between got chars to communicate separately.
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  control_lamp_mqtt(receivedChar);
+  }
+}
  
 void setup() {
   Serial.begin(9600);
