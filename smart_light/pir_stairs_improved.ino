115f17def62e575e4077938e5c2fd3a72e3b5fff
Pawel2357 <2pawel.urbanski@gmail.com>
2021-01-28 14:05:49 +0100

    Debug callback

--- smart_light/pir_stairs_improved.ino
+++ smart_light/pir_stairs_improved.ino
@@ -1,3 +1,5 @@
+#include "DHTesp.h"
+
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -9,12 +11,15 @@ const char* password =    "xyz";
 const char* mqtt_server = "xyz";
 char* MQTT_client =        "pir_stairs";
 char* lamp_control_topic = "lamp_stairs_control";
-int break_time = 10000;
+char* climate_topic =     "data_climate_stairs";
+int break_time = 50000;
 
+DHTesp dht;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
+unsigned long waitCount = 0;                 // counter
+uint8_t conn_stat = 0;                       // Connection status for WiFi and MQTT:
 int inputPin1 = D1;               // choose the input pin (for PIR sensor)
 int inputPin2 = D3;
 int lampPin = D2;
@@ -26,30 +31,12 @@ unsigned long lastStatusLight1 = 0;
 unsigned long lastStatusLight2 = 0;
 unsigned long lastStatusWifi = 0;
 unsigned long lastStatusMqtt = 0;
+unsigned long lastStatusClimate = 0;
 char* pir_1_topic =     "pir_stairs_1";
 char* pir_2_topic =     "pir_stairs_2";
+const int pin_dht = D4;
 
-void setup_wifi2() {
-    delay(10);
-    // We start by connecting to a WiFi network
-    //Serial.println();
-    //Serial.print("Connecting to ");
-    //Serial.println(ssid);
-
-    if (millis() - lastStatusWifi > 500){
-      Serial.println(".");
-      WiFi.mode(WIFI_STA);
-      WiFi.begin(ssid, password);
-      lastStatusWifi = millis();
-  
-      if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("");
-        Serial.println("WiFi connected");
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());
-      }
-    }
-}
+const char* Status = "{\"Message\":\"up and running\"}";
 
 void setup_wifi() {
     delay(10);
@@ -58,42 +45,44 @@ void setup_wifi() {
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    WiFi.mode(WIFI_STA);
+    //WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    //Serial.println("");
+    //Serial.println("WiFi connected");
+    //Serial.println("IP address: ");
+    //Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
-  // Loop until we're reconnected
-  if ((!client.connected()) && (millis() - lastStatusMqtt > 5000)) {
-    Serial.print("Attempting MQTT connection...");
-    yield();
-    lastStatusMqtt = millis();
-    // Attempt to connect
-    if (client.connect(MQTT_client)) {
-      Serial.println("connected");
-      // ... and subscribe to topic
-      client.subscribe(lamp_control_topic);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-    }
-  }
+ // Loop until we're reconnected
+   // Attempt to connect
+ if (client.connect(MQTT_client)) {
+   Serial.println("connected");
+   // ... and subscribe to topic
+   // client.subscribe("lamp_1");
+ }
 }
 
 void publish_data(char* topic, String measure)
 {
   client.publish(topic, (char*) measure.c_str());
+}
+
+void notify_climate(){
+  if (millis() - lastStatusClimate > 2000) {
+    float h = dht.getHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.getTemperature();
+    Serial.print(t);
+    Serial.print(h);
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }else{
+      publish_data(climate_topic, String(h) + "," + String(t) + "," + String(0));
+    }
+    lastStatusClimate = millis();
+  }
 }
 
 void notify_pir_1(){
@@ -168,43 +157,74 @@ void control_lamp_mqtt(String message){
 void callback(char* topic, byte* payload, unsigned int length) {
   // get subscribed message char by char
   // TODO: Do it separately for different topics or add time distance between got chars to communicate separately.
-  String message;
+  String message="";
   for (int i=0;i<length;i++) {
     char receivedChar;
     receivedChar = (char)payload[i];
     message += receivedChar;
   }
+  Serial.print(message);
   control_lamp_mqtt(message);
 }
  
 void setup() {
   Serial.begin(9600);
-  Serial.setTimeout(2000);
+  WiFi.mode(WIFI_STA);
   pinMode(inputPin1, INPUT);     // declare sensor as input
   pinMode(inputPin2, INPUT);     // declare sensor as input
   pinMode(lampPin, OUTPUT);
   digitalWrite(lampPin, HIGH);
-  Serial.begin(9600);
-  
-  setup_wifi();
+  dht.setup(pin_dht, DHTesp::DHT22);
+  client.setCallback(callback);
   client.setServer(mqtt_server, 1883);
 }
  
-void loop(){
-  if ((!client.connected()) && (WiFi.status() != WL_CONNECTED)) {
-    setup_wifi();
-  }else{
-    if (!client.connected()){
+void loop() {                                                     // with current code runs roughly 400 times per second
+// start of non-blocking connection setup section
+  if ((WiFi.status() != WL_CONNECTED) && (conn_stat != 1)) { conn_stat = 0; }
+  if ((WiFi.status() == WL_CONNECTED) && !client.connected() && (conn_stat != 3))  { conn_stat = 2; }
+  if ((WiFi.status() == WL_CONNECTED) && client.connected() && (conn_stat != 5)) { conn_stat = 4;}
+  switch (conn_stat) {
+    case 0:                                                       // MQTT and WiFi down: start WiFi
+      Serial.println("MQTT and WiFi down: start WiFi");
+      setup_wifi();
+      conn_stat = 1;
+      break;
+    case 1:                                                       // WiFi starting, do nothing here
+      Serial.println("WiFi starting, wait : "+ String(waitCount));
+      waitCount++;
+      break;
+    case 2:                                                       // WiFi up, MQTT down: start MQTT
+      Serial.println("WiFi up, MQTT down: start MQTT");
       reconnect();
-    }else{
-      notify_pir_1();
-      notify_pir_2();
-      control_lamp_1();
-      control_lamp_2();
-      client.loop();
-    }
+      conn_stat = 3;
+      waitCount = 0;
+      break;
+    case 3:                                                       // WiFi up, MQTT starting, do nothing here
+      Serial.println("WiFi up, MQTT starting, wait : "+ String(waitCount));
+      waitCount++;
+      break;
+    case 4:                                                       // WiFi up, MQTT up: finish MQTT configuration
+      Serial.println("WiFi up, MQTT up: finish MQTT configuration");
+      client.subscribe(lamp_control_topic);
+      //mqttClient.publish(input_topic, Version);
+      conn_stat = 5;                    
+      break;
   }
-  // Things to do when Wifi is not required
-  //control_lamp_1();
-  //control_lamp_2();
+// end of non-blocking connection setup section
+
+// start section with tasks where WiFi/MQTT is required
+  if (conn_stat == 5) {
+    notify_pir_1();
+    notify_pir_2();                                     
+    notify_climate();
+    client.loop();                                              // internal household function for MQTT
+  } 
+// end of section for tasks where WiFi/MQTT are required
+
+// start section for tasks which should run regardless of WiFi/MQTT
+  control_lamp_1();
+  control_lamp_2();
+  delay(100);
+// end of section for tasks which should run regardless of WiFi/MQTT
 }
