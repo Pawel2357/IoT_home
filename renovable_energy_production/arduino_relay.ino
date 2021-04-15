e5d36c72bf4682ee5fec2fbdb87cdcddfe5e86c7
Pawel2357 <2pawel.urbanski@gmail.com>
2021-04-15 15:21:19 +0200

    Add inverter protection

--- renovable_energy_production/arduino_relay.ino
+++ renovable_energy_production/arduino_relay.ino
@@ -8,6 +8,8 @@ const int pin_6 = 7;
 const int pin_7 = 8;
 const int pin_8 = 9;
 
+boolean inverter_running = false;
+
 void setup() {
   //Serial Begin at 9600 Baud
   pinMode(pin_1, OUTPUT);
@@ -37,40 +39,63 @@ void loop() {
 
     // TODO: iterate over elements in this string or look at nodemcu
 
-    // say what you got:
-    if (a == "1"){
-      digitalWrite(pin_1, LOW);
-      //Serial.print(a);
-    }if (a == "a"){
+    if (a == "7" && inverter_running == false){
       digitalWrite(pin_1, HIGH);
-    }if (a == "2"){
-      digitalWrite(pin_2, LOW);
-    }if (a == "b"){
       digitalWrite(pin_2, HIGH);
-    }if (a == "3"){
-      digitalWrite(pin_3, LOW);
-    }if (a == "c"){
       digitalWrite(pin_3, HIGH);
-    }if (a == "4"){
-      digitalWrite(pin_4, LOW);
-    }if (a == "d"){
       digitalWrite(pin_4, HIGH);
-    }if (a == "5"){
-      digitalWrite(pin_5, LOW);
-    }if (a == "d"){
       digitalWrite(pin_5, HIGH);
-    }if (a == "6"){
-      digitalWrite(pin_6, LOW);
-    }if (a == "e"){
       digitalWrite(pin_6, HIGH);
-    }if (a == "7"){
-      digitalWrite(pin_7, LOW);
-    }if (a == "f"){
-      digitalWrite(pin_7, HIGH);
-    }if (a == "8"){
-      digitalWrite(pin_8, LOW);
-    }if (a == "g"){
       digitalWrite(pin_8, HIGH);
+      delay(100);
+      digitalWrite(pin_7, LOW);
+      delay(1500);
+      inverter_running = true;
+    }if (a == "f"){
+      digitalWrite(pin_1, HIGH);
+      digitalWrite(pin_2, HIGH);
+      digitalWrite(pin_3, HIGH);
+      digitalWrite(pin_4, HIGH);
+      digitalWrite(pin_5, HIGH);
+      digitalWrite(pin_6, HIGH);
+      digitalWrite(pin_8, HIGH);
+      delay(100);
+      digitalWrite(pin_7, HIGH);
+      delay(900);
+      inverter_running = false;
+    }
+    if (inverter_running == true){
+      // say what you got:
+      if (a == "1"){
+        digitalWrite(pin_1, LOW);
+        //Serial.print(a);
+      }if (a == "a"){
+        digitalWrite(pin_1, HIGH);
+      }if (a == "2"){
+        digitalWrite(pin_2, LOW);
+      }if (a == "b"){
+        digitalWrite(pin_2, HIGH);
+      }if (a == "3"){
+        digitalWrite(pin_3, LOW);
+      }if (a == "c"){
+        digitalWrite(pin_3, HIGH);
+      }if (a == "4"){
+        digitalWrite(pin_4, LOW);
+      }if (a == "d"){
+        digitalWrite(pin_4, HIGH);
+      }if (a == "5"){
+        digitalWrite(pin_5, LOW);
+      }if (a == "d"){
+        digitalWrite(pin_5, HIGH);
+      }if (a == "6"){
+        digitalWrite(pin_6, LOW);
+      }if (a == "e"){
+        digitalWrite(pin_6, HIGH);
+      }if (a == "8"){
+        digitalWrite(pin_8, LOW);
+      }if (a == "g"){
+        digitalWrite(pin_8, HIGH);
+      }
     }
   }
 }
