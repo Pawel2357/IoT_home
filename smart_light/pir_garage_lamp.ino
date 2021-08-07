c6fe35da4bf66ddaa1e16fe870802c36bde2e33b
Pawel2357 <2pawel.urbanski@gmail.com>
2021-08-07 10:59:25 +0200

    Update functionality and wifi control.

--- smart_light/pir_garage_lamp.ino
+++ smart_light/pir_garage_lamp.ino
@@ -3,189 +3,179 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            5
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      1
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
 // Connect to the WiFi
-const char* ssid = "xyz";
-const char* password = "xyz";
+const char* ssid =        "xyz";
+const char* password =    "xyz";
 const char* mqtt_server = "xyz";
+char* MQTT_client =        "pir_front_door";
+char* lamp_control_topic = "lamp_front_door";
+int break_time = 30000;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-int n_r = 255;
-int n_g = 255;
-int n_b = 255;
-int n_i = 255;
-int sensor = 13;  // Digital pin D7
+unsigned long waitCount = 0;                 // counter
+uint8_t conn_stat = 0;                       // Connection status for WiFi and MQTT:
+int inputPin1 = D1;               // choose the input pin (for PIR sensor)
+int lampPin = D2;
+bool pirState = LOW;             // we start, assuming no motion detected
+int val1 = 0;                    // variable for reading the pin status
+unsigned long lastStatusLight1 = 0;
+unsigned long lastStatusWifi = 0;
+unsigned long lastStatusMqtt = 0;
+char* pir_1_topic =     "pir_front_door_1";
 
+String work_mode = "a"; // a - auto pir, n - turn off all time, o - on all time
 
-
-/*
- * TrueRandom - A true random number generator for Arduino.
- * This is variant of original work originally implemented as:
- * https://code.google.com/archive/p/tinkerit/ https://github.com/Cathedrow/TrueRandom
- * Copyright (c) 2010 Peter Knight, Tinker.it! All rights reserved.
- * Now modified for the ESP8266
- */
-
-#ifndef ESP8266TrueRandom_h
-#define ESP8266TrueRandom_h
-
-#include <Arduino.h>
-#include <inttypes.h>
-
-class ESP8266TrueRandomClass
-{
-  public:
-  ICACHE_FLASH_ATTR ESP8266TrueRandomClass();
-  ICACHE_FLASH_ATTR int rand();
-  ICACHE_FLASH_ATTR long random();
-  ICACHE_FLASH_ATTR long random(long howBig);
-  ICACHE_FLASH_ATTR long random(long howsmall, long how);
-  ICACHE_FLASH_ATTR int randomBit(void);
-  ICACHE_FLASH_ATTR char randomByte(void);
-  ICACHE_FLASH_ATTR void memfill(char* location, int size);
-  ICACHE_FLASH_ATTR void mac(uint8_t* macLocation);
-  ICACHE_FLASH_ATTR void uuid(uint8_t* uuidLocation);
-  ICACHE_FLASH_ATTR String uuidToString(uint8_t* uuidLocation);
-    bool useRNG;
-  private:
-    unsigned long lastYield;
-    ICACHE_FLASH_ATTR int randomBitRaw(void);
-    ICACHE_FLASH_ATTR int randomBitRaw2(void);
-};
-extern ESP8266TrueRandomClass ESP8266TrueRandom;
-#endif
-
-
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  // get subscribed message char by char
-  // TODO: Do it separately for different topics or add time distance between got chars to communicate separately.
-  payload[length] = '\0'; // Make payload a string by NULL terminating it.
-  String wholeVal = (char *)payload;
-  int number = wholeVal.substring(1, wholeVal.length()).toInt();
-  String data_type = wholeVal.substring(0, 1);
-  Serial.print(number);
-  Serial.println(data_type);
-  String r = String('r');
-  String g = String('g');
-  String b = String('b');
-  String i = String('i');
-  if(data_type == r){
-    n_r = number;
-  }
-  if(data_type == g){
-    n_g = number;
-  }
-  if(data_type == b){
-    n_b = number;
-  }
-  if(data_type == i){
-    n_i = number;
-  }
-
-  pixels.setBrightness(n_i);
-  // Fix note: r swipped with g. This is grb diode.
-  pixels.setPixelColor(0, pixels.Color(n_g, n_r, n_b)); // Moderately bright green color.
-  pixels.show(); // This sends the updated pixel color to the hardware.
-}
+const char* Status = "{\"Message\":\"up and running\"}";
 
 void setup_wifi() {
-
     delay(10);
     // We start by connecting to a WiFi network
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    WiFi.mode(WIFI_STA);
+    //WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    //Serial.println("");
+    //Serial.println("WiFi connected");
+    //Serial.println("IP address: ");
+    //Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
  // Loop until we're reconnected
- while (!client.connected()) {
-   Serial.print("Attempting MQTT connection...");
-   yield();
    // Attempt to connect
- if (client.connect("lamp_1")) {
+ if (client.connect(MQTT_client)) {
    Serial.println("connected");
    // ... and subscribe to topic
-   client.subscribe("lamp_1");
- } else {
-   Serial.print("failed, rc=");
-   Serial.print(client.state());
-   Serial.println(" try again in 5 seconds");
-   // Wait 5 seconds before retrying
-   delay(5000);
-   }
+   // client.subscribe("lamp_1");
  }
 }
 
-
-void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-  pinMode(sensor, INPUT);   // declare sensor as input
-
-  pixels.begin(); // This initializes the NeoPixel library.
-  Serial.begin(9600);
-  //setup_wifi();
-  //client.setServer(mqtt_server, 1883);
-  //client.setCallback(callback);
+void publish_data(char* topic, String measure)
+{
+  client.publish(topic, (char*) measure.c_str());
 }
 
-void loop() {
-    long state = digitalRead(sensor);
-    if(state == HIGH) {
-      Serial.println("Motion detected!");
-      pixels.setBrightness(255);
-      pixels.setPixelColor(0, pixels.Color(random(256), random(256), random(256))); // Moderately bright green color.
-      pixels.show(); // This sends the updated pixel color to the hardware.
-      delay(25000);
-    }
-    else {
-      pixels.setBrightness(0);
-      pixels.setPixelColor(0, pixels.Color(random(256), random(256), random(256))); // Moderately bright green color.
-      pixels.show(); // This sends the updated pixel color to the hardware.
-      Serial.println("Motion absent!");
-      delay(1000);
-    }
-    // Fix note: r swipped with g. This is grb diode.
 
+void notify_pir_1(){
+  val1 = digitalRead(inputPin1);  // read input value
+  if (val1 == HIGH){            // check if the input is HIGH
+    if (pirState == LOW) {
+      Serial.print("motion_start_1");
+      publish_data(pir_1_topic, String("motion_start_1"));
+      pirState = HIGH;
+    }
+  }else{
+    if (pirState == HIGH) {
+      Serial.print("motion_end_1");
+      publish_data(pir_1_topic, String("motion_end_1"));
+      pirState = LOW;
+      }
+    }
+}
 
-  //if (!client.connected()) {
-  //  reconnect();
-  //}
-  //client.loop();
+void control_lamp_1(){
+  if (work_mode == "a"){
+    val1 = digitalRead(inputPin1);  // read input value
+    if (val1 == HIGH){            // check if the input is HIGH
+      lastStatusLight1 = millis();
+      digitalWrite(lampPin, LOW); // turn lamp on
+      }
+    else{
+      if (millis() - lastStatusLight1 > break_time) {
+        digitalWrite(lampPin, HIGH); // turn lamp off
+        }
+      }
+    }else if (work_mode == "n"){
+      digitalWrite(lampPin, HIGH); // turn lamp off
+    }else if (work_mode == "o"){
+      digitalWrite(lampPin, LOW); // turn lamp on
+    }
+  }
+
+void control_lamp_mqtt(String message){
+  Serial.print(message);
+  if(message=="1"){
+    digitalWrite(lampPin, LOW); // turn lamp on
+    lastStatusLight1 = millis();
+  }else{
+    work_mode = message;
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // get subscribed message char by char
+  // TODO: Do it separately for different topics or add time distance between got chars to communicate separately.
+  String message="";
+  for (int i=0;i<length;i++) {
+    char receivedChar;
+    receivedChar = (char)payload[i];
+    message += receivedChar;
+  }
+  Serial.print(message);
+  control_lamp_mqtt(message);
+}
+ 
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  pinMode(inputPin1, INPUT);     // declare sensor as input
+  pinMode(lampPin, OUTPUT);
+  digitalWrite(lampPin, HIGH);
+  client.setCallback(callback);
+  client.setServer(mqtt_server, 1883);
+}
+ 
+void loop() {                                                     // with current code runs roughly 400 times per second
+// start of non-blocking connection setup section
+  if ((WiFi.status() != WL_CONNECTED) && (conn_stat != 1)) { conn_stat = 0; }
+  if ((WiFi.status() == WL_CONNECTED) && !client.connected() && (conn_stat != 3))  { conn_stat = 2; }
+  if ((WiFi.status() == WL_CONNECTED) && client.connected() && (conn_stat != 5)) { conn_stat = 4;}
+  switch (conn_stat) {
+    case 0:                                                       // MQTT and WiFi down: start WiFi
+      Serial.println("MQTT and WiFi down: start WiFi");
+      setup_wifi();
+      conn_stat = 1;
+      break;
+    case 1:                                                       // WiFi starting, do nothing here
+      Serial.println("WiFi starting, wait : "+ String(waitCount));
+      waitCount++;
+      break;
+    case 2:                                                       // WiFi up, MQTT down: start MQTT
+      Serial.println("WiFi up, MQTT down: start MQTT");
+      reconnect();
+      conn_stat = 3;
+      waitCount = 0;
+      break;
+    case 3:                                                       // WiFi up, MQTT starting, do nothing here
+      Serial.println("WiFi up, MQTT starting, wait : "+ String(waitCount));
+      waitCount++;
+      break;
+    case 4:                                                       // WiFi up, MQTT up: finish MQTT configuration
+      Serial.println("WiFi up, MQTT up: finish MQTT configuration");
+      client.subscribe(lamp_control_topic);
+      //mqttClient.publish(input_topic, Version);
+      conn_stat = 5;                    
+      break;
+  }
+// end of non-blocking connection setup section
+
+// start section with tasks where WiFi/MQTT is required
+  if (conn_stat == 5) {
+    notify_pir_1();
+    client.loop();                                              // internal household function for MQTT
+  } 
+// end of section for tasks where WiFi/MQTT are required
+
+// start section for tasks which should run regardless of WiFi/MQTT
+  if (waitCount > 550){
+    ESP.restart();
+  }
+  control_lamp_1();
+  delay(100);
+// end of section for tasks which should run regardless of WiFi/MQTT
 }
